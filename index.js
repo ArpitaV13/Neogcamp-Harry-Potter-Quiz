2f83d3ba086213fc318a14529f24520dc437eac9
Arpita Verma <arpitaverma8885@gmail.com>
2021-08-15 18:21:10 +0000

    Added multi-options

--- index.js
+++ index.js
@@ -21,52 +21,66 @@ console.log(chalk.bold.blue("Welcome ")+chalk.bold.yellow(name)+" !"+"Let us see
 var questions=[
   {
     question:"Name Harry Potter's Parents. ",
-    answer: "James and Lily Potter"
+     options:["James and Lily Potter","Tom and Annie Potter","Ben and James Potter","Brad and Ceasie Potter"],
+    answer:0
   },
   {
     question:"Name of Harry Potter's owl.",
-    answer:"Hedwig"
+    options:["Hedwig","Rabby","Lucy","Jacky"],
+    answer:0
   },
   {
     question:"Draco Malfoy's Dad. ",
-    answer:"Lucius Malfoy"
+    options:["Hagrid","Lucius Malfoy","Ron","Dumbledore"],
+    answer:1
   },
   {
     question:"Name of Lord Voldemort's loyal snake. ",
-    answer:"Nagini"
+    options:["Lizzy","Hissy","Tommy","Nagini"],
+    answer:3
   },
   {
     question:"Name the killing curse. ",
-    answer:"Avada Kedavra"
+    options:["Expelliarmus","Avada Kedavra","Abra-ca-dabra","Expecto Patronum"],
+    answer:1
   },
   {
     question:"Spell for disarming someone. ",
-    answer:"Expelliarmus"
+    options:["Avicii","Expecto Patronum","Abra-ca-Dabra","Expelliarmus"],
+    answer:3
   },
   {
     question:"Harry Potter's godfather. ",
-    answer:"Sirius Black"
+    options:["Draco Malfoy","Hagrid","Sirius Black","Snape"],
+    answer:2
   },
   {
     question:"Ron's pet name. ",
-    answer:"Scabbers"
+    options:["Scabbers","Johny","Tom","Jerry"],
+    answer:0
   },
   {
     question:"Name a muggle.",
-    answer:"Hermione"
+    options:["Draco","Hermione"],
+    answer:1
   },
   {
     question:"Harry Potter's eye colour.",
-    answer:"Green"
+    options:["Brown","Pink","Blue","Green"],
+    answer:3
   }
   
 ];
 
 
 
-function play(question,answer){
-  var userAnswer=readLineSync.question(question);
-  if(userAnswer.toUpperCase() === answer.toUpperCase()) {
+function play(question,options,answer){
+  var optionString=""
+  options.forEach((option,index)=>{
+    optionString+=`\n${index}. ${option}`
+  })
+  var userAnswer=readLineSync.question(`${question}${optionString}\nEnter the option number: `)
+  if(userAnswer=== answer.toString() ){
     console.log(chalk.green("Right"));
     score++;
   }
@@ -82,14 +96,24 @@ function play(question,answer){
 function game(){
   for(var i=0; i<questions.length;i++){
     var currentQuestion=questions[i];
-    play(currentQuestion.question , currentQuestion.answer)
+  
+
+    play(currentQuestion.question ,currentQuestion.options,currentQuestion.answer)
   }
 }
 
 
 
 function showScore(){
-  console.log("Yay! You scored ",score);
+  if(score>=8){
+  console.log("Yay! You know Harry Potter well. Your score is ",score);
+  }
+  else if(score>=5 && score<8){
+    console.log("You may need to watch the episodes again!. Your score is ",score);
+  }
+  else{
+    console.log("You don't know Harry Potter! Your score is ",score);
+  }
 
 }
 
